9b2a14984d94ddbac3f5c001e6c21c48f8e666c7
veronicaperon <160857545+veronicaperon@users.noreply.github.com>
2025-07-04 08:32:15 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto');
+                                                                           .const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
@@ -65,12 +65,12 @@ function classificaSenha(tamanhoAlfabeto) {
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     console.log(entropia);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (entropia > 57) {
+    if (entropia > 11) {
         forcaSenha.classList.add('forte');
     
-    } else if (entropia > 35 && entropia < 57) {
+    } else if (entropia > 5 && entropia < 12) {
         forcaSenha.classList.add('media');
-    } else if (entropia <= 35) {
+    } else if (entropia <= 5 {
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
