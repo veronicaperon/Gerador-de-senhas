68901632ad82c4d82705625bb3eda0368ce367c6
veronicaperon <160857545+veronicaperon@users.noreply.github.com>
2025-07-04 07:48:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,8 @@ function geraSenha() {
     }
     campoSenha.value = senha;
 }   
-function classificaSenha(){
+function classificaSenha(){ 
+    let entropia = tamanhoSenha * Math.log2(alfabeto.length);
 forcaSenha.classList.remove('fraca','media','forte');
 if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
